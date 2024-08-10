6ab7108aed1d5848e345c6b95b6ff285e0fac797
youssef-mohamed-07 <youssefmohamedahmed2004.com>
2024-08-10 16:29:14 +0300

    h

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import Hero from '../components/Home/hero'
-import Statistics from '../components/Home/statistics'
-import Projects from '../components/Home/projects'
-import Services from '../components/Home/services'
-import Testmonials from '../components/Home/testmonials'
-import ContactUs from '../components/Home/contact'
+// src/pages/Home.js
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Hero from '../components/Home/hero';
+import Statistics from '../components/Home/statistics';
+import Projects from '../components/Home/projects';
+import Services from '../components/Home/services';
+import Testmonials from '../components/Home/testmonials';
+import ContactUs from '../components/Home/contact';
+
 export default function Home() {
   return (
     <>
+      <Helmet>
+        <title>Home | Abdul Aziz Groups</title>
+        <meta name="description" content="Welcome to Abdul Aziz Groups. We offer top-notch services in various domains. Explore our projects, statistics, and testimonials to learn more." />
+        <meta name="keywords" content="Abdul Aziz Groups, Services, Projects, Statistics, Testimonials, Contact Us" />
+        <meta property="og:title" content="Home | Abdul Aziz Groups" />
+        <meta property="og:description" content="Welcome to Abdul Aziz Groups. We offer top-notch services in various domains. Explore our projects, statistics, and testimonials to learn more." />
+        <meta property="og:image" content="logo.png" />
+        <meta property="og:url" content="https://www.abdulazizgroups.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Home | Abdul Aziz Groups" />
+        <meta name="twitter:description" content="Welcome to Abdul Aziz Groups. We offer top-notch services in various domains. Explore our projects, statistics, and testimonials to learn more." />
+        <meta name="twitter:image" content="https://www.abdulazizgroups.com/your-image.jpg" />
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Abdul Aziz Groups",
+              "url": "https://www.abdulazizgroups.com",
+              "logo": "logo.png",
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "+1-800-123-4567",
+                "contactType": "customer service"
+              }
+            }
+          `}
+        </script>
+      </Helmet>
       <Hero />
       <Statistics />
       <Services />
@@ -15,5 +47,5 @@ export default function Home() {
       <Testmonials />
       <ContactUs />
     </>
-  )
+  );
 }
